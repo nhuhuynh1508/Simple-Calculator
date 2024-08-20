121cbca0d4e05fd3e46a5d2daa10f45f0d829a35
Miki <tamnhu03@gmail.com>
2024-08-20 14:01:49 +0700

    updatable task field

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const checkboxList = document.getElementById("list");
 
+
 function addTask() {
     const taskValue = inputBox.value.trim();
 
@@ -12,6 +13,14 @@ function addTask() {
     // Get the current time using Moment.js
     const creationTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
+    // create tasks
+    createTicket(taskValue, creationTime);
+
+    // Clear the input field
+    inputBox.value = '';
+}
+
+function createTicket(info, creationTime) {
     // Create a new list item
     let li = document.createElement('li');
     li.className = 'flex items-center bg-gray-100 px-4 py-2 rounded-lg shadow-sm hover:bg-gray-200';
@@ -21,10 +30,12 @@ function addTask() {
     checkbox.type = 'checkbox';
     checkbox.className = 'custom-checkbox';
 
-    // Create a label
+    // Create a text label
     let label = document.createElement('label');
     label.className = 'ml-3 text-lg text-gray-900';
-    label.textContent = taskValue;
+    // edit text
+    label.contentEditable = 'true';
+    label.textContent = info;
 
     // Create a timestamp element
     let timestamp = document.createElement('span');
@@ -34,9 +45,8 @@ function addTask() {
     // Create a remove icon
     let removeIcon = document.createElement('span');
     removeIcon.innerHTML = '&times;';
-    removeIcon.className = 'ml-auto cursor-pointer text-black text-lg hover:text-red-700 font-bold';
-    removeIcon.style.fontSize = '1.25rem';
-    removeIcon.addEventListener('click', function() {
+    removeIcon.className = 'ml-auto cursor-pointer text-black text-2xl hover:text-red-700 font-bold';
+    removeIcon.addEventListener('click', function () {
         li.remove();
     });
 
@@ -48,19 +58,19 @@ function addTask() {
 
     // Append the list item to the list
     checkboxList.appendChild(li);
-
-    // Clear the input field
-    inputBox.value = '';
 }
 
-inputBox.addEventListener("keyup", function(event) {
+// create default tasks
+createTicket("Default Task", moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+inputBox.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
-        event.preventDefault();
+        // event.preventDefault();
         addTask();
     }
 });
 
-checkboxList.addEventListener("click", function(e) {
+checkboxList.addEventListener("click", function (e) {
     if (e.target.tagName === "INPUT" && e.target.type === "checkbox") {
         e.target.parentElement.classList.toggle("checked");
         const label = e.target.nextElementSibling;
@@ -75,7 +85,6 @@ function updateTime() {
     const timeDisplay = document.getElementById('time-display');
     timeDisplay.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
-
 // Update the time every second
 setInterval(updateTime, 1000);
 
